d7d4e07a159f7b415441c029005cf945911f46dc
YZCUS <43754173+YZCUS@users.noreply.github.com>
2023-11-28 21:15:22 -0500

    112823

---
+++ src/chat/ChatClient.java
@@ -0,0 +1,135 @@
+package chat;
+
+import java.awt.*;
+import java.io.*;
+import java.net.Socket;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import javax.crypto.*;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.spec.SecretKeySpec;
+import javax.swing.*;
+
+import encryption.Encryption;
+
+
+public class ChatClient extends JFrame {
+
+    private static final String RSA = "RSA";
+    private static final String SERVER_PUBLIC_KEY = "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGk9wUQ4G9PChyL5SUkCyuHjTNOglEy5h4KEi0xpgjxi/UbIH27NXLXOr94JP1N5pa1BbaVSxlvpuCDF0jF9jlZw5IbBg1OW2R1zUACK+NrUIAYHWtagG7KB/YcyNXHOZ6Icv2lXXd7MbIao3ShrUVXo3u+5BJFCEibd8a/JD/KpAgMBAAE=";
+    private PublicKey serverPublicKey;
+    private Key communicationKey;
+
+    public ChatClient() {
+        super("Chat Client");
+        try {
+            serverPublicKey = Encryption.readPublicKey(SERVER_PUBLIC_KEY);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("error getting server public key: " + e.getMessage());
+        }
+        setSize(500, 700);
+        setTitle("Chat Client");
+        setLayout(new BorderLayout());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem connectServer = new JMenuItem("Connect");
+        menu.add(connectServer);
+        JMenuItem exitApp = new JMenuItem("Exit");
+        menu.add(exitApp);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
+        JPanel displayPanel = new JPanel();
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.setPreferredSize(new Dimension(500, 300));
+        displayPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 10, 20));
+        JTextArea display = new JTextArea();
+        display.setLineWrap(true);
+        display.setWrapStyleWord(true);
+        display.setEditable(false);
+        JScrollPane displayScrollPane = new JScrollPane(display);
+        displayPanel.add(displayScrollPane, BorderLayout.CENTER);
+        add(displayPanel, BorderLayout.CENTER);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BorderLayout());
+        inputPanel.setPreferredSize(new Dimension(500, 50));
+        inputPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+        JTextField input = new JTextField();
+        inputPanel.add(input, BorderLayout.CENTER);
+        JButton sendButton = new JButton("Send");
+        inputPanel.add(sendButton, BorderLayout.EAST);
+        add(inputPanel, BorderLayout.SOUTH);
+
+        connectServer.addActionListener(e -> {
+            try {
+                Socket socket = new Socket("localhost", 9898);
+                DataInputStream inputFromServer = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToServer = new DataOutputStream(socket.getOutputStream());
+                String initialGreetingMessage = "HELLO";
+                outputToServer.writeUTF(initialGreetingMessage);
+                outputToServer.flush();
+                String connectedMessage = inputFromServer.readUTF();
+                if ("CONNECTED".equals(connectedMessage)) {
+                    display.append("Connected To Server\n");
+                    byte[] AESSeed = Encryption.generateSeed();
+                    communicationKey = Encryption.generateAESKey(AESSeed);
+                    byte[] encryptedAESSeed = Encryption.pkEncrypt(serverPublicKey, AESSeed);
+                    outputToServer.writeUTF(Base64.getEncoder().encodeToString(encryptedAESSeed));
+                    outputToServer.flush();
+                } else {
+                    display.append("Error connecting to server\n");
+                    socket.close();
+                }
+                sendButton.addActionListener(sent -> {
+                    String inputMessage = input.getText();
+                    if (!inputMessage.isEmpty()) {
+                        try {
+                            String encryptedMessage = Encryption.encrypt(communicationKey, inputMessage);
+                            display.append("Me: " + inputMessage + "\n");
+                            input.setText("");
+                            outputToServer.writeUTF(encryptedMessage);
+                            outputToServer.flush();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                            System.err.println("error encrypting message: " + ex.getMessage());
+                        }
+                    }
+                });
+                new Thread(() -> {
+                    try {
+                        while (true) {
+                            String encryptedMessage = inputFromServer.readUTF();
+                            String decryptedMessage = Encryption.decrypt(communicationKey, encryptedMessage);
+                            display.append(decryptedMessage + "\n");
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        System.err.println("error connecting to server: " + ex.getMessage());
+                    }
+                }).start();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.err.println("error connecting to server: " + ex.getMessage());
+            }
+
+        });
+
+        exitApp.addActionListener(e -> {
+            System.exit(0);
+        });
+
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        ChatClient chatClient = new ChatClient();
+    }
+}
---
+++ src/chat/ChatServer.java
@@ -0,0 +1,178 @@
+package chat;
+
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Date;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.swing.*;
+
+import encryption.Encryption;
+
+import java.security.*;
+
+
+public class ChatServer extends JFrame implements Runnable {
+
+    private static final String RSA = "RSA";
+    private Key privateKey;
+    private JTextArea dialog = new JTextArea();
+    private int clientCount = 0;
+    private List<ClientHandler> clientHandlers;
+    private static final int PORT = 9898;
+
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            dialog.append("Chat Server started at " + new Date() + '\n');
+            while (true) {
+                Socket socket = serverSocket.accept();
+                clientCount++;
+                dialog.append("Starting thread for client " + clientCount + " at " + new Date() + '\n');
+
+                InetAddress inetAddress = socket.getInetAddress();
+                dialog.append("Client " + clientCount + "'s host name is " + inetAddress.getHostName() + '\n');
+                dialog.append("Client " + clientCount + "'s IP Address is " + inetAddress.getHostAddress() + '\n');
+                ClientHandler newClientHandler = new ClientHandler(socket, privateKey);
+                clientHandlers.add(newClientHandler);
+                new Thread(newClientHandler).start();
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        } finally {
+            clientHandlers.remove(this);
+        }
+    }
+
+    class ClientHandler implements Runnable {
+        private Socket socket;
+        private Key privateKey;
+        private Key communicationKey;
+        private DataOutputStream outputToClient;
+        private final int clientNo;
+
+        public ClientHandler(Socket socket, Key privateKey) throws IOException {
+            this.socket = socket;
+            this.privateKey = privateKey;
+            this.outputToClient = new DataOutputStream(socket.getOutputStream());
+            this.clientNo = clientCount;
+        }
+
+        public void sendMessage(String message) {
+            try {
+                String encryptedResponse = Encryption.encrypt(this.communicationKey, message);
+                this.outputToClient.writeUTF(encryptedResponse);
+            } catch (Exception e) {
+                System.err.println("Error sending message: " + e.getMessage());
+            }
+        }
+
+        public void broadcastMessage(String responseMessage) {
+            synchronized (clientHandlers) {
+                for (ClientHandler clientHandler : clientHandlers) {
+                    if (clientHandler != this) {
+                        clientHandler.sendMessage(responseMessage);
+                    }
+                }
+            }
+        }
+
+        public void run() {
+            try {
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+
+
+                String initialMessage = inputFromClient.readUTF();
+                if (!"HELLO".equals(initialMessage)) {
+                    outputToClient.writeUTF("ERROR");
+                    outputToClient.flush();
+                    socket.close();
+                    return;
+                } else {
+                    outputToClient.writeUTF("CONNECTED");
+                    outputToClient.flush();
+                }
+                byte[] encryptedSeed = Base64.getDecoder().decode(inputFromClient.readUTF());
+                byte[] AESSeed = Encryption.pkDecrypt(privateKey, encryptedSeed);
+                communicationKey = Encryption.generateAESKey(AESSeed);
+                System.out.println("communication key: " + communicationKey);
+                while (true) {
+                    String encryptedMessage = inputFromClient.readUTF();
+                    String decryptedMessage = Encryption.decrypt(communicationKey, encryptedMessage);
+                    String responseMessage = "Client " + clientNo + " sent: " + decryptedMessage;
+                    dialog.append(responseMessage + '\n');
+                    broadcastMessage(responseMessage);
+                }
+            } catch (IOException ioe) {
+                System.err.println(ioe.getMessage());
+            } catch (InvalidAlgorithmParameterException | NoSuchPaddingException | IllegalBlockSizeException |
+                     NoSuchAlgorithmException | BadPaddingException | InvalidKeyException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public ChatServer() {
+
+        super("Chat Server");
+        try {
+            privateKey = Encryption.readPrivateKey("keypairs/pkcs8_key");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("problem loading private key: " + e.getMessage());
+            System.exit(1);
+        }
+        clientHandlers = Collections.synchronizedList(new ArrayList<ClientHandler>());
+        setSize(500, 700);
+        setTitle("Chat Server");
+        setLayout(new BorderLayout());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem runServer = new JMenuItem("Run Server");
+        menu.add(runServer);
+        JMenuItem exitApp = new JMenuItem("Exit");
+        menu.add(exitApp);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
+        JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new BorderLayout());
+        dialogPanel.setPreferredSize(new Dimension(500, 300));
+        dialogPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
+        dialog.setEditable(false);
+        JScrollPane dialogScrollPane = new JScrollPane(dialog);
+        dialogPanel.add(dialogScrollPane, BorderLayout.CENTER);
+        add(dialogPanel, BorderLayout.CENTER);
+
+        runServer.addActionListener(e -> {
+            new Thread(this).start();
+        });
+
+        exitApp.addActionListener(e -> {
+            System.exit(0);
+        });
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+    }
+
+}
+
+
